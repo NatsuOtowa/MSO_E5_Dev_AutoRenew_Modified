63945ea7992bd12f606837927ea7e3b9d438c327
NatsuOtowa <33691095+NatsuOtowa@users.noreply.github.com>
2023-05-30 11:05:46 +0900

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,11 @@ import random
 # mail: Mail.Read、Mail.ReadWrite、MailboxSettings.Read、MailboxSettings.ReadWrite
 # After registration, you must click on behalf of xxx to grant administrator consent, otherwise outlook api cannot be called
 
+
+
+
+
+
 calls = [
     'https://graph.microsoft.com/v1.0/me/drive/root',
     'https://graph.microsoft.com/v1.0/me/drive',
